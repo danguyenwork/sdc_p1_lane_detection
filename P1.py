3c9e8dd01fddc2ad0b8c58198ebf4ab261bf46be
Anh Nguyen <adnguyen1989@gmail.com>
2017-09-11 12:13:42 -0700

    done

--- P1.py
+++ P1.py
@@ -10,6 +10,10 @@ from moviepy.editor import VideoFileClip
 from IPython.display import HTML
 import sys
 
+def to_hsl(img):
+    """Applies the HSL transform"""
+    return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
+
 def grayscale(img):
     """Applies the Grayscale transform
     This will return an image with only one color channel
@@ -19,9 +23,6 @@ def grayscale(img):
 
     return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
-    # or use BGR2GRAY if you read an image with cv2.imread()
-    # return cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
-
 def canny(img, low_threshold, high_threshold):
     """Applies the Canny transform"""
     return cv2.Canny(img, low_threshold, high_threshold)
@@ -29,6 +30,24 @@ def canny(img, low_threshold, high_threshold):
 def gaussian_blur(img, kernel_size):
     """Applies a Gaussian Noise kernel"""
     return cv2.GaussianBlur(img, (kernel_size, kernel_size), 0)
+
+def isolate_colors(img):
+    """Isolate yellow and white to improve recognition
+    img should be in OpenCV HSL format
+    """
+
+    boundaries = [([15, 38, 115], [35, 204, 255]),([0, 200, 0], [180, 255, 255])]
+    outputs = []
+
+    for (lower, upper) in boundaries:
+        lower = np.array(lower, dtype = "uint8")
+        upper = np.array(upper, dtype = "uint8")
+
+        mask = cv2.inRange(img, lower, upper)
+        output = cv2.bitwise_and(img, img, mask = mask)
+        outputs.append(output)
+
+    return sum(outputs)
 
 def region_of_interest(img, vertices):
     """
@@ -54,59 +73,83 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
-def fit_lane_lines(lines):
+
+def extract_potential_lanes(lines):
+    """
+    Helper for draw_lines
+    Eliminate lines where the slopes are above or below a certain threshold.
+    This gets rid of things like shadows, smudges, etc.
+    """
     slopes = (lines[:,3] - lines[:,1]) / (lines[:,2] - lines[:,0])
-    left_slope_index = slopes < 0
-    left_slope = slopes[left_slope_index].mean()
-    right_slope = slopes[~left_slope_index].mean()
+    valid_line_index = (((slopes < .9) & (slopes > .4)) | ((slopes > -.9) & (slopes < -.4)))
+    valid_lines = lines[valid_line_index]
+    return(valid_lines)
 
-    left_b = (lines[left_slope_index][:,1] - lines[left_slope_index][:,0] * left_slope).mean()
-    right_b = (lines[~left_slope_index][:,3] - lines[~left_slope_index][:,2] * right_slope).mean()
+def parse_lane_points(lines):
+    """
+    Helper for draw_lines
+    Prepare data for polyfit
+    Parse the list of lines into four sets of arrays: x and y coordinates of left and right lanes
+    """
+    slopes = (lines[:,3] - lines[:,1]) / (lines[:,2] - lines[:,0])
+    left_lane_index = slopes < 0
+    left_lane_points_x = lines[left_lane_index][:,[0,2]].reshape(1,-1)[0]
+    left_lane_points_y = lines[left_lane_index][:,[1,3]].reshape(1,-1)[0]
+    right_lane_points_x = lines[~left_lane_index][:,[0,2]].reshape(1,-1)[0]
+    right_lane_points_y = lines[~left_lane_index][:,[1,3]].reshape(1,-1)[0]
+    return left_lane_points_x, left_lane_points_y, right_lane_points_x, right_lane_points_y
 
-    return left_slope, right_slope, left_b, right_b
+def draw_hough_line(img, lines, color=[255,0,0], thickness=2):
+    """
+    Draw hough lines without any extrapolation
+    """
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    for line in lines:
+        for x1,y1,x2,y2 in line:
+            cv2.line(line_img, (x1,y1), (x2,y2),[255, 0, 0],2)
+    return line_img
 
 def draw_lines(img, lines, Y, color=[255, 0, 0], thickness=2):
     """
-    NOTE: this is the function you might want to use as a starting point once you want to
-    average/extrapolate the line segments you detect to map out the full
-    extent of the lane (going from the result shown in raw-lines-example.mp4
-    to that shown in P1_example.mp4).
-
-    Think about things like separating line segments by their
-    slope ((y2-y1)/(x2-x1)) to decide which segments are part of the left
-    line vs. the right line.  Then, you can average the position of each of
-    the lines and extrapolate to the top and bottom of the lane.
-
-    This function draws `lines` with `color` and `thickness`.
-    Lines are drawn on the image inplace (mutates the image).
-    If you want to make the lines semi-transparent, think about combining
-    this function with the weighted_img() function below
+    This function extrapolates from a set of lines to arrive at the final lanes.
+    - Eliminate invalid lines
+    - Parse points into left and right lane based on slopes
+    - Polyfit the data to calculate the slope and intercept of each lane
+    - Calculate the top of the region of interest
+    - Calculate the end-points of each lane
+    - Draw each lane on a copy of the image
+    Return a copy of the image with the two lanes drawn
     """
-    lines = lines[:,0]
-    left_slope, right_slope, left_b, right_b = fit_lane_lines(lines)
 
-    # import ipdb; ipdb.set_trace()
-    top_y = lines[:,3].min()
+    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
+    valid_lines = extract_potential_lanes(lines[:,0])
 
-    x_bottom_right = int((Y - right_b) / right_slope)
-    x_bottom_left =  int((Y - left_b) / left_slope)
+    left_lane_points_x, left_lane_points_y, right_lane_points_x, right_lane_points_y = parse_lane_points(valid_lines)
 
-    x_top_right = int((top_y - right_b) / right_slope)
-    x_top_left = int((top_y - left_b) / left_slope)
+    top_y = max(len(left_lane_points_y) != 0 and left_lane_points_y.min() or 0, \
+                len(right_lane_points_y) != 0 and right_lane_points_y.min() or 0)
 
-    cv2.line(img, (x_bottom_right, Y), (x_top_right, top_y), color, thickness)
-    cv2.line(img, (x_bottom_left, Y), (x_top_left, top_y), color, thickness)
+    if len(left_lane_points_x) != 0:
+        left_lane = np.polyfit(left_lane_points_x, left_lane_points_y, 1)
+        x_bottom_left =  int((Y - left_lane[1]) / left_lane[0])
+        x_top_left = int((top_y - left_lane[1]) / left_lane[0])
+        cv2.line(line_img, (x_bottom_left, Y), (x_top_left, top_y), color, thickness)
 
-def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap, Y):
+    if len(right_lane_points_y) != 0:
+        right_lane = np.polyfit(right_lane_points_x, right_lane_points_y, 1)
+        x_bottom_right = int((Y - right_lane[1]) / right_lane[0])
+        x_top_right = int((top_y - right_lane[1]) / right_lane[0])
+        cv2.line(line_img, (x_bottom_right, Y), (x_top_right, top_y), color, thickness)
+    return line_img
+
+def hough_lines(img, rho, theta, threshold, min_line_len, max_line_gap):
     """
     `img` should be the output of a Canny transform.
 
     Returns an image with hough lines drawn.
     """
     lines = cv2.HoughLinesP(img, rho, theta, threshold, np.array([]), minLineLength=min_line_len, maxLineGap=max_line_gap)
-    line_img = np.zeros((img.shape[0], img.shape[1], 3), dtype=np.uint8)
-    draw_lines(line_img, lines, Y, thickness=5)
-    return line_img
+    return lines
 
 def weighted_img(img, initial_img, alpha=0.8, beta=1., lam=0.):
     """
@@ -122,66 +165,94 @@ def weighted_img(img, initial_img, alpha=0.8, beta=1., lam=0.):
     """
     return cv2.addWeighted(initial_img, alpha, img, beta, lam)
 
-def process_image(image):
+
+def process_image(image, filename="default"):
+    """ pipeline for lane detection:
+    - HSL
+    - Isolate yellow / white
+    - Grayscale
+    - Gaussian
+    - Canny
+    - Hough
+    - Lane extrapolation
+    """
+
     copy = np.copy(image)
-    copy = grayscale(copy)
 
-    copy = gaussian_blur(copy, 5)
-
-    copy = canny(copy, 20, 150)
-
+    # ==================
+    # Configurable parameters
     Y = copy.shape[0]
     X = copy.shape[1]
     BOTTOM_LEFT = (int(150 / 960. * X),Y)
-    TOP_LEFT = (int(460 / 960. * X), int(320 / 540. * Y))
-    TOP_RIGHT = (int(500 / 960. * X), int(320 / 540. * Y))
-    BOTTOM_RIGHT = (int(860 / 960. * X),Y)
-
+    TOP_LEFT = (int(450 / 960. * X), int(310 / 540. * Y))
+    TOP_RIGHT = (int(500 / 960. * X), int(310 / 540. * Y))
+    BOTTOM_RIGHT = (int(920 / 960. * X),Y)
     vertices = np.array([[BOTTOM_LEFT, TOP_LEFT, TOP_RIGHT, BOTTOM_RIGHT]], dtype=np.int32)
-    copy = region_of_interest(copy, vertices)
 
-    rho = 1 # distance resolution in pixels of the Hough grid
+    rho = 1   # distance resolution in pixels of the Hough grid
     theta = np.pi/180 # angular resolution in radians of the Hough grid
     threshold = 15    # minimum number of votes (intersections in Hough grid cell)
     min_line_length = 10 #minimum number of pixels making up a line
-    max_line_gap = 100
-    copy = hough_lines(copy, rho, theta, threshold, min_line_length, max_line_gap, Y)
-    copy = weighted_img(copy,image,alpha=0.8, beta=1.2, lam=0.)
+    max_line_gap = 40
+    # ==================
 
+    copy = to_hsl(copy)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_0hsl.png',copy)
+
+    copy = isolate_colors(copy)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_1isolate.png',copy)
+
+    copy = grayscale(copy)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_2grayscale.png',copy,cmap='gray')
+
+    copy = gaussian_blur(copy, 5)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_3gaussian.png',copy,cmap='gray')
+
+    copy = canny(copy, 50, 150)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_4canny.png',copy,cmap='gray')
+
+    copy = region_of_interest(copy, vertices)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_5region.png',copy,cmap='gray')
+
+    lines = hough_lines(copy, rho, theta, threshold, min_line_length, max_line_gap)
+    copy2 = draw_hough_line(copy, lines)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_6hough.png',copy2,cmap='gray')
+
+    copy = draw_lines(copy, lines, Y, thickness=5)
+    filename != 'default' and mpimg.imsave('test_images/' + filename + '_out_7drawline.png',copy,cmap='gray')
+
+    copy = weighted_img(copy,image,alpha=0.8, beta=1.2, lam=0.)
     return copy
 
-def image_test(directory):
+def process_directory(directory):
     arr = os.listdir(directory)
     for filename in arr:
+        print('processing input: ' + filename)
         filename_arg = os.path.splitext(filename)
-        if '_out' not in filename_arg[0] and filename_arg[1] in ['.jpg','.png']:
-            inp = mpimg.imread(directory + filename)
-            output = process_image(inp)
-            output_dir = directory+filename_arg[0]+'_out'+filename_arg[1]
-            mpimg.imsave(output_dir,output)
+        output_dir = directory+filename_arg[0]+'_out_8final'+filename_arg[1]
 
-def video_test(directory):
-    arr = os.listdir(directory)
-    for filename in arr:
-        filename_arg = os.path.splitext(filename)
-        if '_out' not in filename_arg[0] and filename_arg[1] in ['.mp4']:
-            print('processing input: ' + filename)
-            inp = VideoFileClip(directory + filename).subclip(0,0)
-            print('processing lane detection: ' + filename)
+        if '_out' in filename_arg[0]:
+            continue
+        elif filename_arg[1] in ['.jpg','.png']:
+            inp = mpimg.imread(directory + filename)
+            output = process_image(inp, filename_arg[0])
+            mpimg.imsave(output_dir,output)
+        elif filename_arg[1] in ['.mp4']:
+            inp = VideoFileClip(directory + filename)
             output = inp.fl_image(process_image)
-            print('processing output: ' + filename)
-            output_dir = directory+filename_arg[0]+'_out'+filename_arg[1]
             output.write_videofile(output_dir, audio=False)
 
 VID_DIR = "test_videos/"
 IMG_DIR = "test_images/"
 
-
 if __name__ == '__main__':
+    # python P1.py
     if len(sys.argv) == 1:
-        image_test(IMG_DIR)
-        video_test(VID_DIR)
+        process_directory(IMG_DIR)
+        process_directory(VID_DIR)
+    # python P1.py img
     elif sys.argv[1] == 'img':
-        image_test(IMG_DIR)
+        process_directory(IMG_DIR)
+    # python P1.py vid
     elif sys.argv[1] == 'vid':
-        video_test(VID_DIR)
+        process_directory(VID_DIR)
